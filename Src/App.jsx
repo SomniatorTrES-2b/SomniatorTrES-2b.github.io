3b15276e899a5d2156281bd97ada6434fede5685
SomniatorTrES-2b <brennan.roveda@yahoo.com>
2025-07-14 20:11:21 +0900

    update

---
+++ Src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Somniator 💤</h1>
+      <p>Dream.</p>
+      <button>Commencer le rêve</button>
+    </div>
+  );
+}
+
+export default App;
